5dd8657f94cd9d69b8318fa978afe9ffff6afea9
Ruslan1992-lab <ruslan.abkhairov92@gmail.com>
2024-09-28 18:51:47 +0300

    Add files via upload

---
+++ module_1_Переменные.py
@@ -0,0 +1,6 @@
+course_name = 'Курс: Python,'
+completed_homework_count = 12
+spent_hours = 1.5
+average_time_per_task = spent_hours / completed_homework_count  
+
+print(course_name, 'всего задач:', completed_homework_count, ', затрачено часов:', spent_hours, ',', 'среднее время выполнения:', average_time_per_task, 'часа.')
