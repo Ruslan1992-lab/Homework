782b54196bc920e3d4693eca497caf4f406e5684
Ruslan1992-lab <ruslan.abkhairov92@gmail.com>
2024-09-28 16:42:06 +0300

    module_1_Строки и Индексация строк

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 example = 'Топинамбур'
+
 print(example[0])
+
 print(example[-1])
-print(example[5:10])
+
+print(example[4:])
+
 print(example[::-1])
-result=example[1::2]
-print(result)
+
+print(example[1::2])
+
