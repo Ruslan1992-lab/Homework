2868067869895f65b52ef4e47aa9f2ba58cd0118
Ruslan1992-lab <ruslan.abkhairov92@gmail.com>
2024-10-02 00:35:47 +0300

    Add files via upload

---
+++ module_1_5_Неизменяемые и изменяемые обьекты.py
@@ -0,0 +1,10 @@
+immutable_var = (1, 2, 'a', 'b')
+print(immutable_var)
+
+# immutable_var[2] = 2
+# кортеж не поддерживает обрашение по элементам
+
+mutable_list = ['alpha',14 , 16, 'appplle', 'sun' ]
+print(mutable_list)
+mutable_list[2] = 'urban'
+print(mutable_list)
