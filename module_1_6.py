5569f669c510efed621490cfa5e761da83ebe961
Ruslan1992-lab <ruslan.abkhairov92@gmail.com>
2024-10-03 21:57:20 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -2,14 +2,13 @@ my_dict = {'Ruslan': 1992, 'Elina': 1987, 'Evgeniy': 1993}
 print(my_dict)
 print(my_dict.get('Ruslan'))
 print(my_dict.get('Vladimir'))
-my_dict.update({'Luciay': 1991 ,'German': 1996})
-del my_dict['Evgeniy']
+my_dict.update({'Luciay': 1991, 'German': 1996})
+deleted_value = my_dict.pop('Evgeniy')
+print("Удаленное значение:", deleted_value)
 print(my_dict)
-
-
 my_set = {1, 1, 1, 2, 2, 'Pacha', 'Pacha', True, True}
-print(set(my_set))
-print(my_set.add('Kino'))
-print(my_set.add('Boock'))
-print(my_set.discard(True))
+print(set(my_set)) 
+my_set.add('Kino')
+my_set.add('Boock')
+my_set.discard(True)
 print(my_set)
